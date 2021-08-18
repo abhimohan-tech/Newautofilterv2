243a81f017b4af8d93b49dc24e816535c6b6a7af
Master <85671955+abhimohan-tech@users.noreply.github.com>
2021-08-18 13:54:50 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1639,10 +1639,10 @@ async def callback_data(bot, update: CallbackQuery):
 
     if query_data == "start":
         buttons = [[
-            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/AlbertEinstein_TG'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot')
+            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/rocky_boy01'),
+            InlineKeyboardButton('Source Code 🧾', url ='https://t.me/ff_new_movies_group')
         ],[
-            InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+            InlineKeyboardButton('Support 🛠', url='https://t.me/ff_new_movies_group')
         ],[
             InlineKeyboardButton('Help ⚙', callback_data="help")
         ]]
